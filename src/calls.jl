18d68b966f6847fd599f9a5cd6b82d1bfb5692b3
Laurent Bartholdi <laurent.bartholdi@gmail.com>
2016-04-07 18:49:58 +0200

    Improved call to accept any arg number

--- src/calls.jl
+++ src/calls.jl
@@ -12,39 +12,13 @@ end
 
 FuncObj(obj::Obj) = unsafe_load(reinterpret(Ptr{FuncObj},PTR_BAG(obj)))
 
-macro CALL_nARGS(args...)
-    nargs = length(args)
-    quote
-        f = FuncObj($(esc(args[1])))
-        @mark_stack_bottom()
-        ccall(f.handler[$nargs],Obj,$(Expr(:tuple,[Obj for i in 1:nargs]...)),$args...)
-    end
-end
-
-macro CALL_0ARGS(obj)
+macro CALL_nARGS(obj,args...)
+    nargs = 1+length(args)
     quote
         f = FuncObj($(esc(obj)))
         @mark_stack_bottom()
-        ccall(f.handler[1],Obj,(Obj,),$(esc(obj)))
+        ccall(f.handler[$nargs],Obj,$(Expr(:tuple,[Obj for i in 1:nargs]...)),$(esc(obj)),$(args...))
     end
 end
 
-macro CALL_1ARGS(obj,a1)
-    quote
-        f = FuncObj($(esc(obj)))
-        @mark_stack_bottom()
-        ccall(f.handler[2],Obj,(Obj,Obj),$(esc(obj)),$(esc(a1)))
-    end
-end
-
-macro CALL_2ARGS(obj,a1,a2)
-    quote
-        f = FuncObj($(esc(obj)))
-        @mark_stack_bottom()
-        ccall(f.handler[3],Obj,(Obj,Obj,Obj),$(esc(obj)),$(esc(a1)),$(esc(a2)))
-    end
-end
-
-(obj::Obj)() = @CALL_0ARGS(obj)
-(obj::Obj)(a1::Obj) = @CALL_1ARGS(obj,a1)
-(obj::Obj)(a1::Obj,a2::Obj) = @CALL_2ARGS(obj,a1,a2)
+(obj::Obj)(args...) = @CALL_nARGS(obj,map(Obj,args)...)
